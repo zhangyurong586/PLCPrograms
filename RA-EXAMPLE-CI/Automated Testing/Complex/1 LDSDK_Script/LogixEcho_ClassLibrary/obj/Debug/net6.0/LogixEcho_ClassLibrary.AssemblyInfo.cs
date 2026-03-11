eb90bb85bd3a4237f8d44be5f04c3e7787a2b114
Administrator <Administrator@WIN-PANR2QTT91E>
2026-03-11 01:12:10 -0700

    update excel file path

--- RA-EXAMPLE-CI/Automated Testing/Complex/1 LDSDK_Script/LogixEcho_ClassLibrary/obj/Debug/net6.0/LogixEcho_ClassLibrary.AssemblyInfo.cs
+++ RA-EXAMPLE-CI/Automated Testing/Complex/1 LDSDK_Script/LogixEcho_ClassLibrary/obj/Debug/net6.0/LogixEcho_ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843a25a4f2ecb678c349af0ed99cbde566d58e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogixEcho_ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
